756b7cfd110eb69138589e546acef0a822a5a5f5
CaseyNord <nordcw@gmail.com>
2018-11-13 22:39:57 -0800

    major refactoring and improved extendability

---
+++ Bot_Commands.py
@@ -0,0 +1,33 @@
+maps = {
+    "carv": ("rock carving locations", "http://www.powerpyx.com/wp-content/uploads/red-dead-redemption-2-rock-carvings-locations-map.jpg"),
+    "dream": ("dreamcatcher locations", "http://www.powerpyx.com/wp-content/uploads/red-dead-redemption-2-dreamcatcher-locations-map.jpg"),
+    "hadino": ("New Hanover & Ambarino dino bone locations", "https://assets.vg247.com/current/2018/11/red-dead-redemption-2-dinosaur-bones-1-768x501.png"),
+    "wedino": ("West Elizabeth dino bone locations", "https://assets.vg247.com/current/2018/11/red-dead-redemption-2-dinosaur-bones-2-768x445.png"),
+    "nadino": ("New Austin dino bone locations", "https://assets.vg247.com/current/2018/11/red-dead-redemption-2-dinosaur-bones-3-768x467.png0"),
+    "aicigs": ("amazing inventions cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/10/amazing-inventions-cigarette-cards-red-dead-redemption-2-update.jpg"),
+    "awpcigs": ("artists, writers, and poets cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/10/all-artists-writers-and-poets-cigarette-card-locations-map.jpg"),
+    "bhcigs": ("breeds of horses cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/10/all-breeds-of-horses-cigarette-card-maps.jpg"),
+    "fgcigs": ("famous gunslingers cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/10/all-famous-gunslingers-cigarette-card-locations-map.jpg"),
+    "faacigs": ("fauna of north america cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/10/all-fauna-of-north-america-cigarette-cards-location-map.jpg"),
+    "flacigs": ("flora of north america cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-flora-of-north-america-cigarette-cards-location-map.jpg"),
+    "gbcigs": ("gems of beauty cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-gems-of-beauty-cigarette-card-locations-map.jpg"),
+    "mtcigs": ("marvels of time cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-marvels-of-travel-cigarette-card-locations-map.jpg"),
+    "pacigs": ("prominent americans cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-prominent-americans-cigarette-cards-location-map.jpg"),
+    "sscigs": ("stars of the stage cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-stars-of-the-stage-cigarette-card-locations-map.jpg"),
+    "vacigs": ("vistas of america cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-vistas-of-america-cigarette-card-locations-map.jpg"),
+    "wccigs": ("the world's champions cigarette cards locations", "https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-worlds-champions-cigarette-card-locations-map.jpg")
+}
+
+info = {
+    "animals": ("legendary animal information", "if you've gone hunting with Hosea you should have a map in your satchel...\n"
+                                                "if that isn't enough you can find more information here\n"
+                                                "https://www.eurogamer.net/articles/2018-11-09-red-dead-redemption-2-legendary-animal-locations-4975"),
+    "fish": ("legendary fish information", "if you've met with Jeremy Gill you should have a map in your satchel...\n"
+                                           "if that isn't enough you can find more information here\n"
+                                           "https://www.eurogamer.net/articles/2018-11-09-red-dead-redemption-2-legendary-fish-locations-4975"),
+    "ciginfo": ("cigarette card information", "you can scour the land looking for cigarette cards, but there may be an easier way\n"
+                                              "http://www.powerpyx.com/red-dead-redemption-2-all-cigarette-card-locations/"),
+    "mystery": ("mysteries information", "there are a lot of mysteries out there\n"
+                                         "if yer not afraid of spoilin' some fun, you can find learn more here\n"
+                                         "https://www.gamespot.com/videos/uncovering-red-dead-redemption-2s-biggest-mystery/2300-6447057/"),
+}
--- Main.py
+++ Main.py
@@ -6,6 +6,9 @@ from discord import Message
 from discord import Status
 from discord.ext import commands
 
+import Bot_Commands
+
+
 BOT_PREFIX = "!"
 
 # real token/bot
@@ -19,197 +22,89 @@ TOKEN = "NTA5OTU4MDIzMjkzNDM1OTE1.DsVYQA.lluEGtYuhDYTTxOR8t_QN-t6nxU"
 client = commands.Bot(command_prefix=BOT_PREFIX)
 
 
+# - FETCH COMMANDS- #########################################################
+
+def format_commands(commands_dict):
+    command_string = ""
+    for key, value in commands_dict.items():
+        command_string += "*" + key + "* - " + value[0] + "\n"
+    return command_string
+
+
+def query_format_commands(commands_dict, match):
+    command_string = ""
+    for key, value in commands_dict.items():
+        if match in key:
+            command_string += "*" + key + "* - " + value[0] + "\n"
+    return command_string
+
+
+@client.command(name="map", aliases=["m"])
+async def fetch_map(ctx, arg):
+    for key, value in Bot_Commands.maps.items():
+        if key == arg:
+            await ctx.send(value[1])
+
+
+@client.command(name="info", aliases=["i"])
+async def fetch_info(ctx, arg):
+    for key, value in Bot_Commands.info.items():
+        if key == arg:
+            await ctx.send(value[1])
+
+
 # - MENUS - ##################################################################
 
 @client.command()
 async def howdy(ctx):
     """Say 'howdy' when a user types '!howdy' and list basic commands."""
-    await ctx.send("howdy, {0}".format(ctx.author))
-    await ctx.send("i've got maps and information, partner, what're ya lookin' for?")
-    await ctx.send("------")
-    await ctx.send("!maps")
-    await ctx.send("!info")
+    await ctx.send("howdy, {0}".format(ctx.author) + "\n"
+                   "i've got maps and information, partner, what're ya lookin' for?\n"
+                   "------\n"
+                   "!maps\n"
+                   "!links")
 
-
-# - INFO - ###################################################################
 
 @client.command()
-async def info(ctx):
+async def links(ctx):
     """List info commands."""
-    await ctx.send("i've got plenty of information to share, whad'ya want to know?")
-    await ctx.send("!animals - legendary animal information")
-    await ctx.send("!fish - legendary fish information")
-    await ctx.send("!ciginfo - cigarette card information")
-    await ctx.send("!mystery - mysteries information")
+    info_commands = format_commands(Bot_Commands.info)
+    await ctx.send("i've got plenty of information to share, whad'ya want to know?\n"
+                   "------\n" +
+                   info_commands)
 
-
-@client.command(name="animals")
-async def animal_info(ctx):
-    """List animal information."""
-    await ctx.send("if you've gone hunting with Hosea you should have a map in your satchel...")
-    await ctx.send("if that isn't enough you can find more information here")
-    await ctx.send("https://www.eurogamer.net/articles/2018-11-09-red-dead-redemption-2-legendary-animal-locations-4975")
-
-
-@client.command(name="fish")
-async def fish_info(ctx):
-    """List fish information."""
-    await ctx.send("if you've met with Jeremy Gill you should have a map in your satchel...")
-    await ctx.send("if that isn't enough you can find more information here")
-    await ctx.send("https://www.eurogamer.net/articles/2018-11-09-red-dead-redemption-2-legendary-fish-locations-4975")
-
-
-@client.command(name="ciginfo")
-async def cig_info(ctx):
-    """List cigarette card information"""
-    await ctx.send("you can scour the land looking for cigarette cards, but there may be an easier way")
-    await ctx.send("http://www.powerpyx.com/red-dead-redemption-2-all-cigarette-card-locations/")
-
-
-@client.command(name="mystery")
-async def mystery_info(ctx):
-    """List mystery information."""
-    await ctx.send("there are a lot of mysteries out there")
-    await ctx.send("if yer not afraid of spoilin' some fun, you can find learn more here")
-    await ctx.send("https://www.gamespot.com/videos/uncovering-red-dead-redemption-2s-biggest-mystery/2300-6447057/")
-
-
-# - MAPS - ###################################################################
 
 @client.command()
 async def maps(ctx):
     """List map commands."""
-    await ctx.send("there's a lot to find out in the country, here's the maps i got for ya'")
-    await ctx.send("------")
-    await ctx.send("!dream - dreamcatcher locations")
-    await ctx.send("!carvin - rock carving locations")
-    await ctx.send("!cigmaps - cigarette card locations")
-    await ctx.send("!hadino - New Hanover & Ambarino dino bone locations")
-    await ctx.send("!wedino - West Elizabeth dino bone locations")
-    await ctx.send("!nadino - New Austin dino bone locations")
+    map_commands = format_commands(Bot_Commands.maps)
+    await ctx.send("there's a lot to find out in the country, here's the maps i got for ya'\n"
+                   "------\n"
+                   "--type !map *command*\n" +
+                   map_commands)
 
 
 @client.command(name="dino")
 async def dino_maps(ctx):
     """"List dino bones map commands."""
-    await ctx.send("if you're out looking for dino bones, take a look at these maps here")
-    await ctx.send("------")
-    await ctx.send("!hadino - New Hanover & Ambarino dino bone locations")
-    await ctx.send("!wedino - West Elizabeth dino bone locations")
-    await ctx.send("!nadino - New Austin dino bone locations")
+    dino_map_commands = query_format_commands(Bot_Commands.maps, "dino")
+    await ctx.send("if you're out looking for dino bones, take a look at these maps here\n"
+                   "------\n"
+                   "--type !map *command*\n" +
+                   dino_map_commands)
 
 
-@client.command(name="cigmaps")
+@client.command(name="cigs")
 async def cigarette_cards(ctx):
     """List cigarette cards map commands."""
-    await ctx.send("there's a lotta cards out there, which ones are ya lookin' for?")
-    await ctx.send("------")
-    await ctx.send("!aicigs - amazing inventions cigarette cards locations")
-    await ctx.send("!awpcigs - artists, writers, and poets cigarette cards locations")
-    await ctx.send("!bhcigs - breeds of horses cigarette cards locations")
-    await ctx.send("!fgcigs - famous gunslingers cigarette cards locations")
-    await ctx.send("!faacigs - fauna of north america cigarette cards locations")
-    await ctx.send("!flacigs - flora of north america cigarette cards locations")
-    await ctx.send("!gbcigs - gems of beauty cigarette cards locations")
-    await ctx.send("!mtcigs - marvels of time cigarette cards locations")
-    await ctx.send("!pacigs - prominent americans cigarette cards locations")
-    await ctx.send("!sscigs - stars of the stage cigarette cards locations")
-    await ctx.send("!vacigs - vistas of america cigarette cards locations")
-    await ctx.send("!wccigs - the world's champions cigarette cards locations")
+    cigs_map_commands = query_format_commands(Bot_Commands.maps, "cigs")
+    await ctx.send("there's a lotta cards out there, which ones are ya lookin' for?\n"
+                   "------\n"
+                   "--type !map *command*\n" +
+                   cigs_map_commands)
 
 
-# - ROCK CARVINGS - ##########################################################
-
-@client.command(name="carv")
-async def rock_carvings(ctx):
-    await ctx.send("http://www.powerpyx.com/wp-content/uploads/red-dead-redemption-2-rock-carvings-locations-map.jpg".format(ctx))
-
-
-# - DREAMCATCHERS - ##########################################################
-
-@client.command(name="dream")
-async def rock_carvings(ctx):
-    await ctx.send("https://cdn.gamerant.com/wp-content/uploads/red-dead-redemption-2-map-dreamcatchers-notes-729x410.jpg.webp".format(ctx))
-
-
-# - DINO BONES - #############################################################
-
-@client.command(name="hadino")
-async def hanover_ambarino_dino_bones(ctx):
-    await ctx.send("https://assets.vg247.com/current/2018/11/red-dead-redemption-2-dinosaur-bones-1-768x501.png".format(ctx))
-
-
-@client.command(name="wedino")
-async def west_elizabeth_dino_bones(ctx):
-    await ctx.send("https://assets.vg247.com/current/2018/11/red-dead-redemption-2-dinosaur-bones-2-768x445.png".format(ctx))
-
-
-@client.command(name="nadino")
-async def new_austin_dino_bones(ctx):
-    await ctx.send("https://assets.vg247.com/current/2018/11/red-dead-redemption-2-dinosaur-bones-3-768x467.png".format(ctx))
-
-
-# - CIGARETTE CARDS - ########################################################
-
-@client.command(name="aicigs")
-async def amazing_inventions_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/10/amazing-inventions-cigarette-cards-red-dead-redemption-2-update.jpg".format(ctx))
-
-
-@client.command(name="awpcigs")
-async def artists_writers_poets_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/10/all-artists-writers-and-poets-cigarette-card-locations-map.jpg".format(ctx))
-
-
-
-@client.command(name="bhcigs")
-async def breeds_of_horses_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/10/all-breeds-of-horses-cigarette-card-maps.jpg".format(ctx))
-
-
-@client.command(name="fgcigs")
-async def famous_gunslingers_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/10/all-famous-gunslingers-cigarette-card-locations-map.jpg".format(ctx))
-
-
-@client.command(name="facigs")
-async def fauna_of_north_america_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/10/all-fauna-of-north-america-cigarette-cards-location-map.jpg".format(ctx))
-
-
-@client.command(name="flacigs")
-async def flora_of_north_america_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-flora-of-north-america-cigarette-cards-location-map.jpg".format(ctx))
-
-@client.command(name="gbcigs")
-async def gems_of_beauty_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-gems-of-beauty-cigarette-card-locations-map.jpg".format(ctx))
-
-
-@client.command(name="mtcigs")
-async def marvels_of_travel_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-marvels-of-travel-cigarette-card-locations-map.jpg".format(ctx))
-
-
-@client.command(name="pacigs")
-async def prominent_americans_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-prominent-americans-cigarette-cards-location-map.jpg".format(ctx))
-
-
-@client.command(name="sscigs")
-async def stars_of_the_stage_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-stars-of-the-stage-cigarette-card-locations-map.jpg".format(ctx))
-
-
-@client.command(name="vacigs")
-async def vistas_of_america_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-vistas-of-america-cigarette-card-locations-map.jpg".format(ctx))
-
-
-@client.command(name="wccigs")
-async def the_worlds_champions_cards(ctx):
-    await ctx.send("https://d1lss44hh2trtw.cloudfront.net/assets/editorial/2018/11/all-worlds-champions-cigarette-card-locations-map.jpg".format(ctx))
-
+# - BOT COMMANDS - ##################################################################
 
 async def play_games():
     """Allow bot to 'play' game and periodically switch between them."""
@@ -239,41 +134,24 @@ async def play_games():
 
 
 @client.command()
-async def clear(ctx, amount: int):
+async def clear(ctx):
     """Clear messages from channel and re-posts/pins command list."""
-    await ctx.channel.purge(limit=amount)
+    await ctx.channel.purge()
     await asyncio.sleep(3)
-    await ctx.send("**__Bottalo Bill Commands:__**\n"
+    info_commands = format_commands(Bot_Commands.info)
+    map_commands = format_commands(Bot_Commands.maps)
+    await ctx.send("__**Bottalo Bill Commands:**__\n"
                    ".\n"
                    "__Info Commands:__\n"
-                   "!animals - legendary animal information\n"
-                   "!fish - legendary fish information\n"
-                   "!ciginfo - cigarette card information\n"
-                   "!mystery - mysteries information\n"
+                   "--type !info *command*\n" +
+                   info_commands +
                    ".\n"
                    "__Map Commands:__\n"
-                   "!dream - dreamcatcher locations\n"
-                   "!carvin - rock carving locations\n"
-                   "!cigmaps - cigarette card locations\n"
+                   "--type !map *command*\n" +
+                   map_commands +
                    ".\n"
-                   "!hadino - New Hanover & Ambarino dino bone locations\n"
-                   "!wedino - West Elizabeth dino bone locations\n"
-                   "!nadino - New Austin dino bone locations\n"
-                   ".\n"
-                   "!aicigs - amazing inventions cigarette cards locations\n"
-                   "!awpcigs - artists, writers, and poets cigarette cards locations\n"
-                   "!bhcigs - breeds of horses cigarette cards locations\n"
-                   "!fgcigs - famous gunslingers cigarette cards locations\n"
-                   "!faacigs - fauna of north america cigarette cards locations\n"
-                   "!flacigs - flora of north america cigarette cards locations\n"
-                   "!gbcigs - gems of beauty cigarette cards locations\n"
-                   "!mtcigs - marvels of time cigarette cards locations\n"
-                   "!pacigs - prominent americans cigarette cards locations\n"
-                   "!sscigs - stars of the stage cigarette cards locations\n"
-                   "!vacigs - vistas of america cigarette cards locations\n"
-                   "!wccigs - the world's champions cigarette cards locations\n"
-                   ".\n"
-                   "!clear 100 - clear last 100 messages and re-post commands")
+                   "__General Commands:__\n"
+                   "!clear - delete channel messages")
     msg = await ctx.channel.history().get(author__name=BOT_NAME)
     await Message.pin(msg)
 
